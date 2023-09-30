dae5068a789b7bc6690c93ddb3b53160184cd1f4
Anna-Khobta <ana14i88@gmail.com>
2023-09-30 17:53:23 +0300

    Create Friend or Foe? .js

---
+++ Friend or Foe? .js
@@ -0,0 +1,8 @@
+function friend(friends){
+ return friends.filter(name => name.length === 4 )
+}
+
+
+// function friend(friends){  
+// return friends.filter(function (name) { return name.length === 4 });  
+// }
