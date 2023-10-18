92651821448b61a8a8a263599cf25572c925a1b4
Anna-Khobta <ana14i88@gmail.com>
2023-10-18 15:54:14 +0300

    Update Growth of a Population .js

--- Growth of a Population .js
+++ Growth of a Population .js
@@ -6,3 +6,12 @@ function nbYear(p0, percent, aug, p) {
   }
   return years;
 }
+
+
+
+
+function nbYear(p0, percent, aug, p) {
+let year=0;
+   while (p0<p){year++; p0=p0+(p0*(percent/100))+aug}
+   return year
+}
