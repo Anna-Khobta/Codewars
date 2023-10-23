a928df15fc8ff2160ea1d812fe0f7d551943a951
Anna-Khobta <ana14i88@gmail.com>
2023-10-23 21:57:06 +0300

    Update  Reverse words .js

---  Reverse words .js
+++  Reverse words .js
@@ -1,3 +1,16 @@
 function reverseWords(str) {
  return str.split("").reverse().join("").split(" ").reverse().join(" ");
 }
+
+
+
+function reverseWords(str) {
+  return str.split(' ').map(function(word){
+    return word.split('').reverse().join('');
+  }).join(' ');
+}
+
+
+function reverseWords(str) {
+  return str.split(" ").map(x=>x.split("").reverse().join("")).join(" ");
+}
