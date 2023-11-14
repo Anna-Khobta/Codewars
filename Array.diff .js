cc9bf419cf1c1cf3dddde9e7076de2d8e5f54bbb
Anna-Khobta <ana14i88@gmail.com>
2023-11-14 10:36:56 +0300

    Update Array.diff .js

--- Array.diff .js
+++ Array.diff .js
@@ -16,3 +16,7 @@ return returnArray;
 function array_diff(a, b) {
   return a.filter(e => !b.includes(e));
 }
+
+function array_diff(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1; });
+}
