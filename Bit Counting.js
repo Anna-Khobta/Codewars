b0c8d4ee8f4ce9ae02651cd6e585cd078b94a9c8
Anna-Khobta <ana14i88@gmail.com>
2023-11-21 14:44:35 +0300

    Create Bit Counting.js

---
+++ Bit Counting.js
@@ -0,0 +1,20 @@
+let countBits = function(n) {
+  let count = 0;
+  let num = n.toString(2).split('')
+  for (let i = 0; i < num.length; i++) {
+   if (num[i] === '1') {
+     count++
+   }
+  }
+  return count;
+};
+
+
+
+let countBits = function(n) {
+  let count = 0;
+   n.toString(2).split('').forEach(element => {
+    if (element === '1') count++;
+  });
+  return count;
+};
