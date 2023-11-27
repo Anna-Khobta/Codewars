e0e85e23f75c76f32f083161671bf0c3f7aef7e3
Anna-Khobta <ana14i88@gmail.com>
2023-11-27 19:37:04 +0300

    Update Bit Counting.js

--- Bit Counting.js
+++ Bit Counting.js
@@ -11,7 +11,7 @@ let countBits = function(n) {
 
 
 
-let countBits = function(n) {
+let countBits2 = function(n) {
   let count = 0;
    n.toString(2).split('').forEach(element => {
     if (element === '1') count++;
@@ -20,10 +20,10 @@ let countBits = function(n) {
 };
 
 
-countBits = n => n.toString(2).split('0').join('').length;
+countBits3 = n => n.toString(2).split('0').join('').length;
 
 
-const countBits = n => n.toString(2)
+const countBits4 = n => n.toString(2)
                         .split('')
                         .filter(ele => ele == 1)
                         .length
